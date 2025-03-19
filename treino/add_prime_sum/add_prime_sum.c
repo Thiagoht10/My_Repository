f323ddcaba344217c8b2cb1834a4a7c0f063e065
Thiago Souza <thiagodesouzaht@gmail.com>
2025-03-19 12:08:17 +0000

    add_prime

--- treino/add_prime_sum/add_prime_sum.c
+++ treino/add_prime_sum/add_prime_sum.c
@@ -1,16 +1,48 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   add_prime_sum.c                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: thde-sou <thde-sou@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/18 18:38:26 by thde-sou          #+#    #+#             */
-/*   Updated: 2025/03/18 21:17:37 by thde-sou         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int	ft_atoi(char *str)
+{
+	int	a;
+	int	n;
+
+	a = 0;
+	n = 0;
+	while (str[a])
+	{
+		n = n * 10 + (str[a] - '0');
+		a++;
+	}
+	return (n);
+}
+
+char	*putnbr(int num, char *nbr)
+{
+	int	a;
+	int	b;
+	char	c;
+
+	a = 0;
+	while (num > 0)
+	{
+		nbr[a] = (num % 10) + '0';
+		num /= 10;
+		a++;
+	}
+	nbr[a] = '\0';
+	a--;
+	b = 0;
+	while (b < a)
+	{
+		c = nbr[a];
+		nbr[a] = nbr[b];
+		nbr[b] = c;
+		a--;
+		b++;
+	}
+	return (nbr);
+}
 
 int	prime(int a)
 {
@@ -30,6 +62,8 @@ int	add_prime_sum(int num)
 {
 	int	a;
 	int	p;
+	int	i;
+	char	c[10];
 
 	a = 2;
 	p = 0;
@@ -38,12 +72,27 @@ int	add_prime_sum(int num)
 		if(!prime(a))
 			p = p + a;
 		a++;
-	}		
-	return (p);
+	}
+	putnbr(p, c);
+	i = 0;
+	while (c[i])
+		write(1, &c[i++], 1);
+	return (0);
 }
 
-int	main(void)
+int	main(int argc, char **argv)
 {
-	printf("%d\n", add_prime_sum(7));
+	if (argc != 2)
+	{
+		write(1, "0\n", 2);
+		return (0);
+	}
+	if (ft_atoi(argv[1]) < 2)
+	{
+		write(1, "0\n", 2);
+		return (0);
+	}
+	add_prime_sum(ft_atoi(argv[1]));
+	write(1, "\n", 1);
 	return (0);
 }
