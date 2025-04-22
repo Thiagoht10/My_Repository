601d9d40a39b50e1da6b42722c7c7697a9af2600
thiagoht10 <thiagodesouzaht@gmail.com>
2025-04-23 00:25:24 +0100

    home

--- 42lisboa/Common/Libft/ft_strjoin.c
+++ 42lisboa/Common/Libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: thde-sou <thde-sou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/22 21:30:30 by thde-sou          #+#    #+#             */
-/*   Updated: 2025/04/22 22:37:28 by thde-sou         ###   ########.fr       */
+/*   Updated: 2025/04/23 00:22:55 by thde-sou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,16 @@
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	size_t	len;
-	char	*str1;
-	char	*str2;
+	char	*res;
 
 	len = ft_strlen(s1) + ft_strlen(s2) + 1;
-	str1 = malloc(len);
-	str2 = ft_calloc(ft_strlen(s2) + 1, sizeof(char));
-	ft_memmove(str1, s1, ft_strlen(s1));
-	ft_memmove(str2, s2, ft_strlen(s2));
-	if (!str1)
+	res = malloc(len);
+	if (!res)
 		return (NULL);
-	ft_strlcat((char *)str1, str2, len);
-	return (str1);
+	ft_memmove(res, s1, ft_strlen(s1));
+	res[ft_strlen(s1)] = '\0';
+	ft_strlcat((char *)res, s2, len);
+	return (res);
 }
 
 /*int     main(void)
