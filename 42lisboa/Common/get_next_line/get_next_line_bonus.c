4926da82b7854cf5e1b5b675a210837922373236
Thiagoht10 <thiagodesouzaht@gmail.com>
2025-05-23 21:20:47 +0100

    42

--- 42lisboa/Common/get_next_line/get_next_line_bonus.c
+++ 42lisboa/Common/get_next_line/get_next_line_bonus.c
@@ -1,37 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: thde-sou <thde-sou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/17 19:16:56 by thde-sou          #+#    #+#             */
-/*   Updated: 2025/05/23 19:43:01 by thde-sou         ###   ########.fr       */
+/*   Updated: 2025/05/23 21:03:30 by thde-sou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
 char	*get_next_line(int fd)
 {
-	static char	buffer[BUFFER_SIZE + 1];
+	static char	buffer[MAX_FD][BUFFER_SIZE + 1];
 	char		*linha;
 	size_t		feedback;
 
 	linha = NULL;
 	while (1)
 	{
-		feedback = build_line(buffer, &linha, fd);
+		feedback = build_line(buffer[fd], &linha, fd);
 		if (feedback == 0)
 			break ;
 		if (feedback == 2)
 			return (NULL);
-		if (newline(buffer))
+		if (newline(buffer[fd]))
 		{
-			found_rest(buffer);
+			found_rest(buffer[fd]);
 			break ;
 		}
-		buffer[0] = '\0';
+		buffer[fd][0] = '\0';
 	}
 	if (linha && *linha)
 		return (linha);
@@ -42,10 +42,17 @@ char	*get_next_line(int fd)
 int	main(void)
 {
 	int		fd;
+	int		fd2;
 	char	*lido;
 
 	fd = open("arquivo.txt", O_RDONLY);
+	fd2 = open("arquivo2.txt", O_RDONLY);
 	if (fd < 0)
+	{
+		printf("%s\n", "ABERTURA DO ARQUIVO FALHOU!");
+		return (1);
+	}
+	if (fd2 < 0)
 	{
 		printf("%s\n", "ABERTURA DO ARQUIVO FALHOU!");
 		return (1);
@@ -53,10 +60,13 @@ int	main(void)
 	lido = get_next_line(fd);
 	printf("%s", lido);
 	free(lido);
-	lido = get_next_line(fd);
+	lido = get_next_line(fd2);
 	printf("%s", lido);
 	free(lido);
 	lido = get_next_line(fd);
+	printf("%s", lido);
+	free(lido);
+	lido = get_next_line(fd2);
 	printf("%s", lido);
 	free(lido);
 	close(fd);
---
+++ 42lisboa/Common/get_next_line/get_next_line_bonus.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thde-sou <thde-sou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/17 18:53:17 by thde-sou          #+#    #+#             */
+/*   Updated: 2025/05/23 21:02:59 by thde-sou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+
+#ifndef MAX_FD
+#  define MAX_FD 1024
+# endif
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 9
+# endif
+
+char	*get_next_line(int fd);
+void	found_rest(char *buffer);
+size_t	ft_strlen(char const *s);
+int		newline(char *s);
+char	*ft_strjoin(char *s1, char const *s2);
+int		build_line(char *buffer, char **linha, int fd);
+
+#endif
