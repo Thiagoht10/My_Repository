01faa4e5903fb91a449c460678986daac076493b
thiagoht10 <thiagodesouzaht@gmail.com>
2025-07-30 00:00:55 +0100

    push_swap

---
+++ 42lisboa/Common/push_swap/make_stack.c
@@ -0,0 +1,104 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   make_stack.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thde-sou <thde-sou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/25 15:45:04 by thde-sou          #+#    #+#             */
+/*   Updated: 2025/07/26 16:03:47 by thde-sou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	count_num(int argc, char **argv)
+{
+	int	found;
+	int	arg;
+	int	i;
+
+	found = 0;
+	arg = 1;
+	while (arg < argc)
+	{
+		i = 0;
+		while (argv[arg][i] == 9 || argv[arg][i] == 32)
+			i++;
+		while (1)
+		{
+			if (argv[arg][i] == 9 || argv[arg][i] == 32 || argv[arg][i] == '\0')
+			{
+				if (argv[arg][i - 1] != 9 && argv[arg][i - 1] != 32)
+					found++;
+			}
+			if (argv[arg][i] == '\0')
+				break ;
+			i++;
+		}
+		arg++;
+	}
+	return (found);
+}
+
+void	copy_stack(int *res, int *stack_a, int *len, int *i)
+{
+	int	j;
+
+	j = 0;
+	while (j < *len)
+	{
+		stack_a[*i] = res[j];
+		(*i)++;
+		j++;
+	}
+}
+
+int	*make_stack(int argc, char **argv, int *len)
+{
+	int	*stack_a;
+	int	arg;
+	int	i;
+	int	len_split;
+	int	*res;
+
+	i = 0;
+	arg = 1;
+	*len = count_num(argc, argv);
+	stack_a = malloc(*len * sizeof(int));
+	while (arg < argc)
+	{
+		res = make_split(argv[arg], &len_split);
+		copy_stack(res, stack_a, &len_split, &i);
+		arg++;
+		free(res);
+	}
+	return (stack_a);
+}
+
+t_stack	*create_stack(int size)
+{
+	t_stack	*stack;
+
+	stack = malloc(sizeof(t_stack));
+	if (!stack)
+		return (NULL);
+	stack->items = malloc(size * sizeof(int));
+	if (!stack->items)
+	{
+		free(stack);
+		return (NULL);
+	}
+	stack->size = size;
+	stack->count = 0;
+	return (stack);
+}
+
+void	free_stack(t_stack *stack)
+{
+	if (stack)
+	{
+		free(stack->items);
+		free(stack);
+	}
+}
---
+++ 42lisboa/Common/push_swap/push_swap.h
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thde-sou <thde-sou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/23 09:11:24 by thde-sou          #+#    #+#             */
+/*   Updated: 2025/07/29 21:52:39 by thde-sou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include "libft/libft.h"
+# include <stdio.h>
+
+typedef struct s_stack
+{
+	int	*items;
+	int	size;
+	int	count;
+}		t_stack;
+
+int		str_to_num(char *str);
+int		split_len(char **arr);
+int		*make_split(char *str, int *len);
+int		is_valid_digit(int argc, char **argv);
+int		count_num(int argc, char **argv);
+int		*make_stack(int argc, char **argv, int *len);
+t_stack	*create_stack(int size);
+void	free_stack(t_stack *stack);
+void	sa(t_stack *a);
+void	sb(t_stack *b);
+void	pb(t_stack *a, t_stack *b);
+void	pa(t_stack *a, t_stack *b);
+void	ra(t_stack *a);
+void	rra(t_stack *a);
+void	rb(t_stack *b);
+void	rrb(t_stack *b);
+int		*dp(t_stack *a, int len);
+int		*prev(t_stack *a, int len);
+int		max_seq(t_stack *a, int len, int *max_index);
+int		*lis(t_stack *a, int len);
+int		*rotate(int *f_element, t_stack *a, int len, int *size);
+#endif
